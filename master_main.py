35d15374db8ed782a97b2b4488c4b9883a2403de
pikerpoler <nadav.nissim@gmail.com>
2024-12-16 11:29:13 +0200

    adding buttons

--- master_main.py
+++ master_main.py
@@ -1,12 +1,16 @@
 import bluetooth
-
+from gpiozero import Button
 
 from constants import slave_mac_adresses, communication_port, START, RESET, QUIT
+
+START_PIN = 18
+QUIT_PIN = 23
 
 
 # Create a socket and send the message
 def send_signal(message):
     for mac_address in slave_mac_adresses:
+        print(f"Sending signal to {mac_address}")
         try:
             sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
             sock.connect((mac_address, communication_port))
@@ -17,17 +21,22 @@ def send_signal(message):
             print(f"Bluetooth error: {e}")
 
 
+def start():
+    print("start foo")
+    send_signal(START)
+
+
+def quit():
+    print("quit foo")
+    send_signal(QUIT)
+
+
 if __name__ == "__main__":
-    signal = input("Enter 'start' or 'reset': ").strip().lower()
-    if signal == "start":
-        send_signal(START)
-        # TODO: start the video
-    elif signal == "reset":
-        # TODO: reset the video
-        send_signal(RESET)
-    elif signal == "quit":
-        # TODO: reset the video
-        send_signal(QUIT)
-        exit()
-    else:
-        print("Invalid input!")
+    start_button = Button(START_PIN)
+    quit_button = Button(QUIT_PIN)
+
+    start_button.when_pressed = start
+    quit_button.when_pressed = quit
+
+    while True:
+        pass
