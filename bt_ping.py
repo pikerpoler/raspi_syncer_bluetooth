4a3350c2cd8c68eef9805e59758cc29086d252a9
pikerpoler <nadav.nissim@gmail.com>
2024-12-16 19:15:46 +0200

    added a ping test

---
+++ bt_ping.py
@@ -0,0 +1,83 @@
+import bluetooth
+import time
+import threading
+
+from constants import communication_port, slave_mac_adresses
+from utils import make_device_discoverable
+
+
+def connect_to_server():
+    server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    server_sock.bind(("", communication_port))
+    server_sock.listen(communication_port)
+    print("Waiting for connection...")
+
+    client_sock, client_info = server_sock.accept()
+    print(f"Connection accepted from {client_info}")
+    server_sock.close()
+    return client_sock
+
+
+def connect_to_client():
+    sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    sock.connect((slave_mac_adresses[0], communication_port))
+    return sock
+
+
+def send_signal(message, sock):
+    try:
+        sock.send(message)
+    except bluetooth.BluetoothError as e:
+        print(f"Bluetooth error: {e}")
+
+
+def receive_signal(sock):
+    try:
+        data = sock.recv(1024).decode("utf-8")
+        return data
+
+    except bluetooth.BluetoothError as e:
+        print(f"Bluetooth error: {e}")
+    except KeyboardInterrupt:
+        print("Keyboard interrupt received. Exiting... (is this really necessary?)")
+        exit()
+
+
+def main():
+    make_device_discoverable()
+
+    client_sock = None
+    try:
+        client_sock = connect_to_client()
+    except bluetooth.BluetoothError as e:
+        print(f"Bluetooth error: {e}")
+
+    server_sock = connect_to_server()
+
+    if client_sock is None:
+        client_sock = connect_to_client()
+
+    def worker_function():
+        while True:
+            message = receive_signal(server_sock)
+            send_signal(message, client_sock)
+
+    # Create a thread and mark it as daemon
+    worker_thread = threading.Thread(target=worker_function)
+    worker_thread.daemon = (
+        True  # This ensures the thread terminates with the main process
+    )
+    worker_thread.start()
+
+    while True:
+        user_input = input("Enter a message: ")
+        start = time.time()
+        send_signal(user_input, client_sock)
+        message = receive_signal(server_sock)
+        if message == user_input:
+            end = time.time()
+            print(f"Message sent: {user_input}. Time elapsed: {end - start}")
+
+
+if __name__ == "__main__":
+    main()
