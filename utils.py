bca55552bc1d88f86a1ae0d7b9bee2846ee946d2
pikerpoler <nadav.nissim@gmail.com>
2024-12-16 17:28:29 +0200

    setting the device to auto accept connections

--- utils.py
+++ utils.py
@@ -10,6 +10,10 @@ def make_device_discoverable():
         subprocess.run(["bluetoothctl", "power", "on"], check=True)
         subprocess.run(["bluetoothctl", "discoverable", "on"], check=True)
         subprocess.run(["bluetoothctl", "pairable", "on"], check=True)
+
+        subprocess.run(["bluetoothctl", "agent", "NoInputNoOutput"], check=True)
+        subprocess.run(["bluetoothctl", "default-agent"], check=True)
+
         print("Device is now discoverable and pairable.")
     except subprocess.CalledProcessError as e:
         print(f"Error enabling discoverability: {e}")
