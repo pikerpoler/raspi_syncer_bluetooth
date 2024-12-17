f72ddf0963f1c4ef4ce4bcd500e09f6a4c2b7b9b
pikerpoler <nadav.nissim@gmail.com>
2024-12-17 16:29:00 +0200

    deps

--- script/dependency_installation.sh
+++ script/dependency_installation.sh
@@ -10,7 +10,12 @@ sudo apt install -y bluetooth bluez python3-bluez
 
 # Install OMXPlayer
 echo "Installing OMXPlayer..."
-sudo apt install -y omxplayer
+# sudo apt install -y omxplayer
+
+wget http://archive.raspberrypi.org/debian/pool/main/o/omxplayer/omxplayer_20190723+gitf543a0d-1_armhf.deb
+sudo dpkg -i omxplayer_20190723+gitf543a0d-1_armhf.deb
+sudo apt-get -f install
+rm omxplayer_20190723+gitf543a0d-1_armhf.deb
 
 # Install Python dependencies with pip3
 echo "Installing Python dependencies with pip3..."
